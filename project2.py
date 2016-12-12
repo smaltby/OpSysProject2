ef79acae5300dd533731712ab24c42deb45f1f70
Sean Maltby <seanmaltby@gmail.com>
2016-12-12 15:26:18 -0500

    Further fixes

--- project2.py
+++ project2.py
@@ -27,6 +27,10 @@ class Process:
         self.arrival_times = arrival_times
 
 
+    def __lt__(self, other):
+         return self.id < other.id
+
+
 def main():
     global n
     if len(sys.argv) != 3:
@@ -235,70 +239,122 @@ def simulate(processes, algorithm):
     process_partitions = {}
     process_end_times = {}
 
+    def choose_partition(process):
+        chosen_partition = None
+        if algorithm == NEXT_FIT:
+            # Iterate over empty partitions and choose the first one found that fits the process
+            for i in range(len(empty_partitions)):
+                start, end = empty_partitions[i]
+                partition_size = end - start
+                # When an empty partition is selected, resize or delete it accordingly
+                if partition_size > process.size:
+                    empty_partitions[i] = (start + process.size, end)
+                    chosen_partition = (start, start + process.size)
+                    break
+                elif partition_size == process.size:
+                    del empty_partitions[i]
+                    chosen_partition = (start, end)
+                    break
+        elif algorithm == BEST_FIT or algorithm == WORST_FIT:
+            # Iterate over empty partitions and keep track of either the best or worst fit using chosen_i
+            # Replace chosen_i whenever a more suitable candidate is found. When the iteration is done, the
+            # best candidate will have been selected.
+            chosen_i = None
+            for i in range(len(empty_partitions)):
+                start, end = empty_partitions[i]
+                partition_size = end - start
+                if partition_size >= process.size:
+                    if chosen_partition is not None:
+                        if algorithm == BEST_FIT and chosen_partition[1] - chosen_partition[0] <= partition_size:
+                            continue
+                        elif algorithm == WORST_FIT and chosen_partition[1] - chosen_partition[0] >= partition_size:
+                            continue
+
+                    chosen_partition = (start, start + process.size)
+                    chosen_i = i
+
+            # When an empty partition is selected, resize or delete it accordingly
+            if chosen_partition is not None:
+                start, end = empty_partitions[chosen_i]
+                partition_size = end - start
+                if partition_size > process.size:
+                    empty_partitions[chosen_i] = (start + process.size, end)
+                else:
+                    del empty_partitions[chosen_i]
+        return chosen_partition
+
     defragment_time = 0
     t = 0
     print "time %dms: Simulator started (%s)" % (t, algorithm)
     while 1:
         # Check arrivals and ends
+        arrivals = []
+        ends = []
         for process in processes:
             # Process is done, take it out of memory
             if process.id in process_end_times and process_end_times[process.id] == t:
-                # Add the used partition back as an empty partition and delete it's memory
-                start, end = process_partitions[process.id]
-                for i in range(start, end):
-                    memory[i] = None
-
-                # Reverse loop over empty partitions so that if any are deleted, they don't disrupt the loop
-                for i in reversed(range(len(empty_partitions))):
-                    partition = empty_partitions[i]
-                    # Check for adjacencies, and merge with the newly empty partition
-                    if start == partition[1]:
-                        start = partition[0]
-                        del empty_partitions[i]
-                    elif end == partition[0]:
-                        end = partition[1]
-                        del empty_partitions[i]
-                del process_partitions[process.id]
-                empty_partitions.appendleft((start, end))
-
-                print "time %dms: Process %s removed:" % (t + defragment_time, process.id)
-                print_memory(memory)
+                ends.append(process)
 
             # Process has arrived, attempt to add it to memory
             if t in process.arrival_times:
-                print "time %dms: Process %s arrived (requires %d frames)" % (
-                t + defragment_time, process.id, process.size)
-                chosen_partition = choose_partition(algorithm, process, empty_partitions)
+                arrivals.append(process)
 
-                # Could not find an empty partition of sufficient size
-                if chosen_partition is None:
-                    free = free_memory(empty_partitions)
-                    # If there is enough free memory to fit the process, defragment, otherwise, skip the process
-                    if free >= process.size:
-                        print "time %dms: Cannot place process %s -- starting defragmentation" % \
-                              (t + defragment_time, process.id)
-                        frames_moved = defragment(memory, free, process_partitions, empty_partitions)
-                        defragment_time += len(frames_moved) * t_memmove
-                        print "time %dms: Defragmentation complete (moved %d frames: %s)" % \
-                              (t + defragment_time, len(frames_moved), ', '.join(sorted(list(set(frames_moved)))))
-                        print_memory(memory)
+        for process in sorted(ends):
+            # Add the used partition back as an empty partition and delete it's memory
+            start, end = process_partitions[process.id]
+            for i in range(start, end):
+                memory[i] = None
 
-                        # Choose the partition again now that memory has been defragmented
-                        chosen_partition = choose_partition(algorithm, process, empty_partitions)
-                    else:
-                        print "time %dms: Cannot place process %s -- skipped!" % (t + defragment_time, process.id)
-                        print_memory(memory)
-                        continue
+            # Reverse loop over empty partitions so that if any are deleted, they don't disrupt the loop
+            for i in reversed(range(len(empty_partitions))):
+                partition = empty_partitions[i]
+                # Check for adjacencies, and merge with the newly empty partition
+                if start == partition[1]:
+                    start = partition[0]
+                    del empty_partitions[i]
+                elif end == partition[0]:
+                    end = partition[1]
+                    del empty_partitions[i]
+            del process_partitions[process.id]
+            empty_partitions.appendleft((start, end))
 
-                # Update memory
-                for i in range(chosen_partition[0], chosen_partition[1]):
-                    memory[i] = process.id
-                process_partitions[process.id] = chosen_partition
-                print "time %dms: Placed process %s:" % (t + defragment_time, process.id)
-                print_memory(memory)
+            print "time %dms: Process %s removed:" % (t + defragment_time, process.id)
+            print_memory(memory)
 
-                # Record when the process will end
-                process_end_times[process.id] = t + process.arrival_times[t]
+        for process in sorted(arrivals):
+            print "time %dms: Process %s arrived (requires %d frames)" % (
+            t + defragment_time, process.id, process.size)
+            chosen_partition = choose_partition(process)
+
+            # Could not find an empty partition of sufficient size
+            if chosen_partition is None:
+                free = free_memory(empty_partitions)
+                # If there is enough free memory to fit the process, defragment, otherwise, skip the process
+                if free >= process.size:
+                    print "time %dms: Cannot place process %s -- starting defragmentation" % \
+                          (t + defragment_time, process.id)
+                    frames_moved = defragment(memory, free, process_partitions, empty_partitions)
+                    defragment_time += len(frames_moved) * t_memmove
+                    print "time %dms: Defragmentation complete (moved %d frames: %s)" % \
+                          (t + defragment_time, len(frames_moved), ', '.join(sorted(list(set(frames_moved)))))
+                    print_memory(memory)
+
+                    # Choose the partition again now that memory has been defragmented
+                    chosen_partition = choose_partition(process)
+                else:
+                    print "time %dms: Cannot place process %s -- skipped!" % (t + defragment_time, process.id)
+                    print_memory(memory)
+                    continue
+
+            # Update memory
+            for i in range(chosen_partition[0], chosen_partition[1]):
+                memory[i] = process.id
+            process_partitions[process.id] = chosen_partition
+            print "time %dms: Placed process %s:" % (t + defragment_time, process.id)
+            print_memory(memory)
+
+            # Record when the process will end
+            process_end_times[process.id] = t + process.arrival_times[t]
 
         # Check if all processes have completed
         if len(empty_partitions) > 0 and empty_partitions[0] == (0, total_mem):
@@ -313,51 +369,6 @@ def simulate(processes, algorithm):
         # Increment time
         t += 1
     print "time %dms: Simulator ended (%s)" % (t + defragment_time, algorithm)
-
-
-def choose_partition(algorithm, process, empty_partitions):
-    chosen_partition = None
-    if algorithm == NEXT_FIT:
-        # Iterate over empty partitions and choose the first one found that fits the process
-        for i in range(len(empty_partitions)):
-            start, end = empty_partitions[i]
-            partition_size = end - start
-            # When an empty partition is selected, resize or delete it accordingly
-            if partition_size > process.size:
-                empty_partitions[i] = (start + process.size, end)
-                chosen_partition = (start, start + process.size)
-                break
-            elif partition_size == process.size:
-                del empty_partitions[i]
-                chosen_partition = (start, end)
-                break
-    elif algorithm == BEST_FIT or algorithm == WORST_FIT:
-        # Iterate over empty partitions and keep track of either the best or worst fit using chosen_i
-        # Replace chosen_i whenever a more suitable candidate is found. When the iteration is done, the
-        # best candidate will have been selected.
-        chosen_i = None
-        for i in range(len(empty_partitions)):
-            start, end = empty_partitions[i]
-            partition_size = end - start
-            if partition_size >= process.size:
-                if chosen_partition is not None:
-                    if algorithm == BEST_FIT and chosen_partition[1] - chosen_partition[0] <= partition_size:
-                        continue
-                    elif algorithm == WORST_FIT and chosen_partition[1] - chosen_partition[0] >= partition_size:
-                        continue
-
-                chosen_partition = (start, start + process.size)
-                chosen_i = i
-
-        # When an empty partition is selected, resize or delete it accordingly
-        if chosen_partition is not None:
-            start, end = empty_partitions[chosen_i]
-            partition_size = end - start
-            if partition_size > process.size:
-                empty_partitions[chosen_i] = (start + process.size, end)
-            else:
-                del empty_partitions[chosen_i]
-    return chosen_partition
 
 
 def print_memory(memory):
